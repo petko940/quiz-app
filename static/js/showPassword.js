ca061f8a33ae718827f61322e32315a1cabeee33
Petko Todorov <p.todorov1015@gmail.com>
2024-02-08 13:52:01 +0200

    minor fix

--- static/js/showPassword.js
+++ static/js/showPassword.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", function () {
     togglePassword.addEventListener("click", function () {
         if (passwordId.type === "password") {
             passwordId.type = "text";
-            togglePassword.innerHTML = '<i class="fas fa-eye-slash select-none" aria-hidden="true"></i>';
+            togglePassword.innerHTML = '<i class="fas fa-eye-slash" aria-hidden="true"></i>';
         } else {
             passwordId.type = "password";
-            togglePassword.innerHTML = '<i class="fas fa-ey select-none" aria-hidden="true"></i>';
+            togglePassword.innerHTML = '<i class="fas fa-eye" aria-hidden="true"></i>';
         }
     });
 });
